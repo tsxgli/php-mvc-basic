5cf5cae16bc2fc20e7ee676ec71b1a8d0f7ec940
tsxgli <89969164+tsxgli@users.noreply.github.com>
2022-12-29 15:52:47 +0100

    fixed register

--- app/repositories/registerrepository.php
+++ app/repositories/registerrepository.php
@@ -7,27 +7,26 @@ class RegisterRepository extends Repository
     function registerUser(User $user)
     {
         try {
-                $stmt = $this->connection->prepare('INSERT INTO Users (_id,firstname, lastname,email, password, isAdmin, address,postcode, birthdate) 
-                                                    VALUES (null, :lastname, :email, :password, :isAdmin, :address, :postcode, :birthdate);');
-            
-                $stmt->bindValue(':email', $user->getEmail(),PDO::PARAM_STR);
-                $stmt->bindValue(':address', $user->getAddress(),PDO::PARAM_STR);
-                $stmt->bindValue(':password', $user->getPassword(),PDO::PARAM_STR);
-                $stmt->bindValue(':postcode', $user->getPostCode(),PDO::PARAM_STR);
-                $stmt->bindValue(':lastname', $user->getLastName(),PDO::PARAM_STR);
-            $stmt->bindValue(':firstname', $user->getFirstName(), PDO::PARAM_STR);
-                $stmt->bindValue(':birthdate', $user->getBirthdate(),PDO::PARAM_STR);
-                $stmt->bindValue(':isAdmin', $user->getIsAdmin(),PDO::PARAM_BOOL);
-            
-                $stmt->execute();
+            $stmt = $this->connection->prepare('INSERT INTO Users (firstname, lastname,email, password, isAdmin, address,postcode, birthdate) 
+                                                    VALUES ( :firstname,:lastname, :email, :password, :isAdmin, :address, :postcode, :birthdate);');
 
-          
+            $stmt->bindValue(':email', $user->getEmail(), PDO::PARAM_STR);
+            $stmt->bindValue(':address', $user->getAddress(), PDO::PARAM_STR);
+            $stmt->bindValue(':password', $user->getPassword(), PDO::PARAM_STR);
+            $stmt->bindValue(':postcode', $user->getPostCode(), PDO::PARAM_STR);
+            $stmt->bindValue(':lastname', $user->getLastName(), PDO::PARAM_STR);
+            $stmt->bindValue(':firstname', $user->getFirstName(), PDO::PARAM_STR);
+            $stmt->bindValue(':birthdate', $user->getBirthdate(), PDO::PARAM_STR);
+            $stmt->bindValue(':isAdmin', $user->getIsAdmin(), PDO::PARAM_BOOL);
+
+            $stmt->execute();
+
             echo "registered bro";
-        
-        } catch(PDOException $e){
+
+        } catch (PDOException $e) {
             echo "Registering user failed: " . $e->getMessage();
-        }   
-        
+        }
+
     }
 
 }
