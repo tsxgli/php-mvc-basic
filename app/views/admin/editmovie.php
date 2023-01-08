8fd3ee350a251718d96364932a2e871ec5d2a5d9
tsxgli <89969164+tsxgli@users.noreply.github.com>
2023-01-08 23:21:35 +0100

    little update with edit

--- app/views/admin/editmovie.php
+++ app/views/admin/editmovie.php
@@ -16,49 +16,49 @@ include __DIR__ . '/../header.php';
 <body>
     <section>
         <div class="container">
-            
+
             <div class="row">
                 <div class="col">
                     <img src="/images<?='/'. $model[0]->getImage()?>" alt="<?php $title?>" class="img-thumbnail"
                         style="height: 100% ;">
                 </div>
                 <div class="col">
-                    
-                </div>
                 <div class="row">
-                <h3 class="pr-1">Title</h3>
+                    <h3 class="pr-1">Title</h3>
                     <input class="text-center" value="<?php echo $model[0]->getTitle();?>"></input>
                 </div>
                 <div class="row">
                     <h3 class="pr-1">Description</h3>
                     <input class="text-left ml-20" value="<?php echo $model[0]->getDescription();?>"></input>
-                    <div class="row">
-                        <h3>Director</h3>
-                        <input value="<?php echo $model[0]->getDirector();?>"></input>
-                    </div>
-                    <div class="row">
-                        <h3>Date Produced</h3>
-                        <input value="<?php echo $model[0]->getDateProduced();?>"></input>
-                    </div>
-                    <div class="row">
-                        <h3>Genre</h3>
-                        <input value="<?php echo $model[0]->getGenre();?>"></input>
-                    </div>
-                    <div class="row">
-                        <h3>Rating</h3>
-                        <input value="<?php echo $model[0]->getRating();?>"></input>
-                    </div>
-                    <div class="row">
-                        <h3>Price</h3>
-                        <input value=" <?= $model[0]->getPrice()?>">
-                    </div>
-                    <div class="row">
-                        <form action="/cart?id=<?php echo $model[0]->get_id();?>" method="POST">
-                            <button type="submit" class="btn btn-warning" name="buyMovieBtn">
-                               Update</button>
-                        </form>
-                    </div>
                 </div>
+                <div class="row">
+                    <h3>Director</h3>
+                    <input value="<?php echo $model[0]->getDirector();?>"></input>
+                </div>
+                <div class="row">
+                    <h3>Date Produced</h3>
+                    <input value="<?php echo $model[0]->getDateProduced();?>"></input>
+                </div>
+                <div class="row">
+                    <h3>Genre</h3>
+                    <input value="<?php echo $model[0]->getGenre();?>"></input>
+                </div>
+                <div class="row">
+                    <h3>Rating</h3>
+                    <input value="<?php echo $model[0]->getRating();?>"></input>
+                </div>
+                <div class="row">
+                    <h3>Price</h3>
+                    <input value=" <?= $model[0]->getPrice()?>">
+                </div>
+                <div class="row">
+                    <form action="/cart?id=<?php echo $model[0]->get_id();?>" method="POST">
+                        <br><button type="submit" class="btn btn-warning" name="buyMovieBtn">
+                            Update</button>
+                    </form>
+                </div>
+                </div>
+                
                 <div class="col"></div>
             </div>
         </div>
