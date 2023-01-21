b89ef61a700411cf847d2092388dd809aa6c5e13
tsxgli <89969164+tsxgli@users.noreply.github.com>
2023-01-21 21:04:52 +0100

    did last input validation

--- app/views/admin/adduser.php
+++ app/views/admin/adduser.php
@@ -23,14 +23,14 @@ include __DIR__ . '/../header.php';
                     <div class="row">
                         <div class="form-group col-12 col-md-6">
                             <label for="addPostcode">Postcode</label>
-                            <input type="text" class="form-control" id="addPostcode" name="addPostcode" required>
-                        </div>
+                            <input type="text" class="form-control" id="addPostcode" name="addPostcode"  pattern="^[0-9]{4}[A-Za-z]{2}$"required>
+                        <div class="invalid-feedback">Please enter a valid postcode.</div>
                     </div>
                     <div class="row">
                         <div class="form-group col-12 col-md-6">
                             <label for="addAddress">Address</label>
-                            <input type="text" class="form-control" id="addAddress" name="addAddress" required>
-                        </div>
+                            <input type="text" class="form-control" id="addAddress" name="addAddress" pattern="[a-zA-Z]+ [0-9]{1,4}" required>
+                        <div class="invalid-feedback">Please enter a valid address.</div>
                     </div>
                     <div class="row">
                         <div class="form-group col-12 col-md-6">
@@ -41,31 +41,34 @@ include __DIR__ . '/../header.php';
                     <div class="row">
                         <div class="form-group col-12 col-md-6">
                             <label for="addEmail">Email</label>
-                            <input type="text" class="form-control" id="addEmail" name="addEmail" required>
+                            <input type="text" class="form-control" id="addEmail" name="addEmail" required
+                                pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$">
+                            <div class="invalid-feedback">Please enter a valid email.</div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="form-group col-12 col-md-6">
-                            <label for="addPassword">Password</label>
-                            <input type="password" class="form-control" id="addPassword" name="addPassword" required>
-                            <button type="button" class="btn btn-secondary" id="passwordToggleButton"
-                                onclick="passwordToggleButton()">
-                                Show password
-                            </button>
+                        <div class="row">
+                            <div class="form-group col-12 col-md-6">
+                                <label for="addPassword">Password</label>
+                                <input type="password" class="form-control" id="addPassword" name="addPassword"
+                                    required>
+                                <button type="button" class="btn btn-secondary" id="passwordToggleButton"
+                                    onclick="passwordToggleButton()">
+                                    Show password
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="form-group col-12 col-md-6">
-                            <label for="addisAdmin">Is Admin</label>
-                            <select class="form-control" id="addisAdmin" name="addisAdmin" required>
-                                <option value="false">False</option>
-                                <option>True</option>
-                            </select>
-                        </div>
-                     
-                        <div class="form-group">
-                        <br><br> <button type="submit" class="btn btn-warning" id="addUserBtn" name="addUserBtn">Add User</button>
-                        </div>
+                        <div class="row">
+                            <div class="form-group col-12 col-md-6">
+                                <label for="addisAdmin">Is Admin</label>
+                                <select class="form-control" id="addisAdmin" name="addisAdmin" required>
+                                    <option value="false">False</option>
+                                    <option>True</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <br><br> <button type="submit" class="btn btn-warning" id="addUserBtn"
+                                    name="addUserBtn">Add User</button>
+                            </div>
             </form>
         </div>
     </div>
--- app/views/register/index.php
+++ app/views/register/index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,7 +8,7 @@
 </head>
 
 <body>
-    <form method="POST">
+    <form class="needs-validation"method="POST">
         <section class="h-100 bg-dark">s
             <div class="container py-5 h-100">
                 <div class="row d-flex justify-content-center align-items-center h-100">
@@ -17,8 +16,7 @@
                         <div class="card card-registration my-4">
                             <div class="row g-0">
                                 <div class="col-xl-6 d-none d-xl-block">
-                                    <img src="https://play-lh.googleusercontent.com/gIiZK9ZUU7tt8N08ozfe8OPBnW-KPREAHdbN1WDvqqSscVdAQuuP2_i2c1Gm2zHJ_S9x"
-                                        alt="Sample photo" class="img-fluid"
+                                    <img src="/images/wmovies.png" alt="Sample photo" class="img-fluid"
                                         style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem;" />
                                 </div>
                                 <div class="col-xl-6">
@@ -29,65 +27,68 @@
                                             <div class="form-outline mb-4">
                                                 <div class="form-outline">
                                                     <input type="text" id="firstnameRegister"
-                                                        class="form-control form-control-lg" name="firstnameRegister" />
+                                                        class="form-control form-control-lg" name="firstnameRegister"
+                                                        required placeholder="Enter your first name" />
                                                     <label class="form-label" for="firstnameInput">First name</label>
                                                 </div>
                                             </div>
-
-
                                             <div class="row">
                                                 <div class="form-outline mb-4">
                                                     <div class="form-outline">
                                                         <input type="text" id="lastNameRegister"
-                                                            class="form-control form-control-lg" name="lastnameRegister" />
-                                                        <label class="form-label" for="lastNameInput">Last
-                                                            name</label>
+                                                            class="form-control form-control-lg" name="lastnameRegister"
+                                                            required placeholder="Enter your last name" />
+                                                        <label class="form-label" for="lastNameInput">Last name</label>
                                                     </div>
                                                 </div>
-
                                             </div>
-
                                             <div class="form-outline mb-4">
                                                 <input type="text" id="postcodeRegister"
-                                                    class="form-control form-control-lg" name="postcodeRegister" />
+                                                    class="form-control form-control-lg" name="postcodeRegister"
+                                                    required placeholder="Enter your post code"/>
                                                 <label class="form-label" for="postcodeInput">Post Code</label>
                                             </div>
                                             <div class="form-outline mb-4">
                                                 <input type="text" id="addressInput"
-                                                    class="form-control form-control-lg" name="addressRegister" />
+                                                    class="form-control form-control-lg" name="addressRegister" required
+                                                    placeholder="Enter your address" />
                                                 <label class="form-label" for="addressInput">Address</label>
                                             </div>
-
                                             <div class="form-outline mb-4">
-                                                <input type="text" id="emailRegister" class="form-control form-control-lg"
-                                                    name="emailRegister" />
+                                                <input type="text" id="emailRegister"
+                                                    class="form-control form-control-lg" name="emailRegister" required
+                                                    placeholder="Enter your email" />
                                                 <label class="form-label" for="emailInput">Email</label>
                                             </div>
-                                            <div class="form-outline mb-4">
-                                                <input type="date" id="birthdateInput"
-                                                    class="form-control form-control-lg" name="birthdateRegister" />
-                                                <label class="form-label" for="birthdateInput">BirthDate</label>
-                                            </div>
-
-
-                                            <!-- Password field -->
-
                                             <div class="form-group">
                                                 <div class="input-group">
                                                     <input type="password" class="form-control form-control-lg"
-                                                        id="passwordInput" name="passwordRegister">
+                                                        id="passwordInput" name="passwordRegister" required
+                                                        placeholder="Enter your password">
                                                     <div class="input-group-append">
                                                         <button type="button" class="btn btn-secondary"
-                                                            id="passwordToggleButton">
-                                                            Show password
-                                                        </button>
+                                                            id="passwordToggleButton">Show password</button>
                                                     </div>
                                                 </div>
-                                                <label for="passwordInput" class="form-control-label">Password</label>
+                                                <label class="form-label" for="passwordInput">Password</label>
                                             </div>
-                                            <script>
+                                            <div class="form-outline mb-4">
+                                                <input type="date" id="birthdateInput"
+                                                    class="form-control form-control-lg" name="birthdateRegister"
+                                                    required placeholder="Enter your birthdate" />
+                                                <label class="form-label" for="birthdateInput">BirthDate</label>
+                                            </div>
+                                            
+                                        </div>
+
+                                        <div class="d-flex justify-content-end pt-3">
+                                            <a type="button" class="btn btn-light btn-lg" href="/login">Cancel</a>
+                                            <button name="registerBtn" type="submit"
+                                                class="btn btn-warning btn-lg ms-2">Register</button>
+                                        </div>
+                                        <script>
                                             document.getElementById('passwordToggleButton').addEventListener('click',
-                                                function() {
+                                                function () {
                                                     var passwordInput = document.getElementById('passwordInput');
                                                     var passwordToggleButton = this;
 
@@ -99,21 +100,14 @@
                                                         passwordToggleButton.innerHTML = 'Show password';
                                                     }
                                                 });
-                                            </script>
-                                            <!--End password field -->
-
-                                            <div class="d-flex justify-content-end pt-3">
-                                                <a type="button" class="btn btn-light btn-lg" href="/login">Cancel</a>
-                                                <button name="registerBtn" type="submit"
-                                                    class="btn btn-warning btn-lg ms-2">Register</button>
-                                            </div>
-                                        </div>
+                                        </script>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
         </section>
     </form>
 </body>
